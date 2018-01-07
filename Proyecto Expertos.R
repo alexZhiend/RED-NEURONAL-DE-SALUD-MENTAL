ac39179945d580a9653260e34690db75f91dff16
alexZhiend <35173802+alexZhiend@users.noreply.github.com>
2018-01-06 19:53:54 -0500

    Update Proyecto Expertos.R

--- Proyecto Expertos.R
+++ Proyecto Expertos.R
@@ -1,30 +1,51 @@
-
+#PROYECTO DE SISTEMAS EXPERTOS
+#INGENIERIA DE SISTEMAS SERIE 400
+#ALAIN ALEXIS CAMPOS YARANGA
+#YOBETH MARIA MARQUINA CHAVEZ
+#ZORAIDA JESUS SANCHEZ QUISPE
+##############################
+#IMPORTAMOS LA LIBREA CON LA CUAL TRABAJAREMOS
 library(neuralnet)
-setwd("C:\\Users\\user\\Documents\\R\\RED-NEURONAL-DE-SALUD-MENTAL")
-mental1 = read.csv("salud1.csv", header=T)
+#REDIRECCION AL FILE, PARA PODER LEER EL DATASET
+setwd("C:\\Users\\HP\\Downloads")
+mental1 = read.csv("SALUD.csv", header=T)
 str(mental1)
 summary(mental1)
 head(mental1)
 mental1
-ment=model.matrix(~treatment +Age + Gender + family_history + work_interfere, d=mental1)
+#YA QUE EL DATASET ESTA DEFINIDO CON VARIABLES CUALITATIVAS, TRANSFORMAMOS ESTAS VARIABLES
+#A UNA VARIABLES CUATITATIVAS CON LA FUNCION SIGUIENTE
+ment=model.matrix(~treatment + Age + Gender + self_employed + family_history 
+                  + work_interfere + remote_work + tech_company + obs_consequence,d=mental1)
+
+#CONVERSION DE LA MATRIZ CREADA EN UN DATAFRAME
 ment=as.data.frame(ment)
 
-fold.test <- sample(nrow(ment), nrow(ment) / 3)
-test <- ment[fold.test, ]
-train <- ment[-fold.test, ]
+#DISTRIBUIMOS LOS DATOS DE ENTRENAMIENTO Y TESTING
+fold.test <- sample(nrow(ment), nrow(ment)* 0.75)
+train <- ment[fold.test, ]
+test <- ment[-fold.test, ]
 
-nuevo<-neuralnet(treatmentYes ~ Age + GenderFemale + GenderMale +
-                  family_historyYes + work_interfereOften +
-                  work_interfereRarely+work_interfereSometimes,d=train, hidden = 10,threshold = 0.01)
+#CREACION DEL MODELO DE LA RED NEURONAL
+nuevo<-neuralnet(treatmentYes ~ Age + GenderFemale + GenderMale + self_employedOther +
+                   self_employedYes + family_historyYes + work_interfereOften +
+                  work_interfereRarely+work_interfereSometimes+remote_workYes+
+                   tech_companyYes + obs_consequenceYes,d=train, hidden = 10,stepmax = 1e6)
 
+#GRAFICAMOS LA RED NEURONAL
 plot(nuevo)
 
-out = compute(nuevo, test[,c("Age", "GenderFemale", 
-                              "GenderMale", "family_historyYes","work_interfereOften"
-                              ,"work_interfereRarely","work_interfereSometimes")])$net.result
-# Tabla de confusión
+#CAPTURAR LOS DATOS DEL DATAFRAME DE SALIDA
+out = compute(nuevo, test[,c("Age", "GenderFemale","GenderMale" ,"self_employedOther",
+                             "self_employedYes",
+                             "family_historyYes","work_interfereOften","work_interfereRarely",
+                             "work_interfereSometimes","remote_workYes",
+                             "tech_companyYes","obs_consequenceYes")])$net.result
+
+# TABLA DE CONFUSION
 out = out>0.5
 table(out, test[,ncol(test)])
 
-# Porcentaje de acierto
+# PORCENTAJE DE ACIERTO
 mean(out==test[,2])
+
